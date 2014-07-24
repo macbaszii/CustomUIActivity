0dcd53b723694236132baeb01e2ae002bc1de554
Bas <bas@oozou.com>
2014-07-24 17:03:59 +0700

    Initial Commit

---
+++ CustomUIActivity/OZViewController.h
@@ -0,0 +1,13 @@
+//
+//  OZViewController.h
+//  CustomUIActivity
+//
+//  Created by Kiattisak Anoochitarom on 7/24/2557 BE.
+//  Copyright (c) 2557 Kiattisak Anoochitarom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OZViewController : UIViewController
+
+@end
---
+++ CustomUIActivityTests/CustomUIActivityTests.m
@@ -0,0 +1,34 @@
+//
+//  CustomUIActivityTests.m
+//  CustomUIActivityTests
+//
+//  Created by Kiattisak Anoochitarom on 7/24/2557 BE.
+//  Copyright (c) 2557 Kiattisak Anoochitarom. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CustomUIActivityTests : XCTestCase
+
+@end
+
+@implementation CustomUIActivityTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
